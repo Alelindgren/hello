7eb1d00ac216044e1ba6b5d87b69e518403be124
Alex <alindgren6@huskers.unl.edu>
2024-08-29 10:58:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Alex Lindgren
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello my name is Alex!\n");
+
+    return 0;
+}
+
